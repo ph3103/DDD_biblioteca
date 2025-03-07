e3863e63bf068a1d16ec9a2b45a9b72a35db2d76
Eliane <eliane@fiap.com>
2025-03-07 09:49:59 -0300

    Comandos de entrada,saída e processamento

---
+++ ddd-aula02/src/Principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Comentário de linha
+        /*
+        Comentário de bloco
+        Comando de saída -> interação entre
+        o sistema e o usuário
+        Tudo que for exibido
+        * */
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Bom dia!");
+        System.out.println("Digite seu nome: ");
+        String nome, telefone;
+        int idade;
+        nome = leitor.nextLine();
+        System.out.println("Bom dia " + nome +"!");
+        System.out.println("Digite sua idade: ");
+        idade= leitor.nextInt();
+    }
+
+}
---
+++ ddd-aula02/src/Processamento.java
@@ -0,0 +1,10 @@
+public class Processamento {
+    public static void main(String[] args) {
+        int  y = 2;
+        double x = 5, res = x/y;
+        System.out.println(x + "/" + y + " = " + res);
+        res = x % y;
+        System.out.println(x + "%" + y + " = " + res);
+
+    }
+}
